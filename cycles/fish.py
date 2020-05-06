6848b02573639ec3db1286a29a9aee0f423aba30
eriszhang <jiayieris.zhang@mail.utoronto.ca>
2020-05-06 03:15:44 -0400

    separate rendering

--- cycles/fish.py
+++ cycles/fish.py
@@ -29,42 +29,16 @@ my_dir = os.listdir('../meshes/fish/') # dir is your directory path
 num_files = len(my_dir) - 1
 
 
-## initialize blender: rendering set up
-imgRes_x = 900 # recommend > 2000 (UI: Scene > Output > Resolution X)
-imgRes_y = 660 # recommend > 2000 (UI: Scene > Output > Resolution Y)
-numSamples = 100 # recommend > 200 for paper images (UI: Scene > Render > Sampling > Render)
-exposure = 1.5 # exposure of the entire image (UI: Scene > Render > Film > Exposure)
-blenderInit(imgRes_x, imgRes_y, numSamples, exposure)
-
-
-## End material
-###########################################
-
-## set invisible plane (shadow catcher): control the brightness of the shadow
-invisibleGround(shadowBrightness=0.8)
-
-## set camera (recommend to change mesh instead of camera, unless you want to adjust the Elevation)
-camLocation = (2,1.85,0.32)
-lookAtLocation = (0,0,0.5)
-focalLength = 45 # (UI: click camera > Object Data > Focal Length)
-cam = setCamera(camLocation, lookAtLocation, focalLength)
-# bpy.context.object.data.type = 'ORTHO'
-
-## set light
-## Option1: Three Point Light System (recommended)
-# setLight_threePoints(radius=4, height=10, intensity=2500, softness=8, keyLoc='left')
-## Option2: simple sun light
-lightAngle = (-45,0,-45)
-strength = 2
-shadowSoftness = 1
-sun = setLight_sun(lightAngle, strength, shadowSoftness)
-
-## set ambient light
-setLight_ambient(color=(0.1,0.1,0.1,1)) # (UI: Scene > World > Surface > Color)
-
-img_idx = [1,96,120,180]
+img_idx = [180]
 
 for i in range(len(img_idx)):
+
+    ## initialize blender: rendering set up
+    imgRes_x = 1350 # recommend > 2000 (UI: Scene > Output > Resolution X)
+    imgRes_y = 990 # recommend > 2000 (UI: Scene > Output > Resolution Y)
+    numSamples = 100 # recommend > 200 for paper images (UI: Scene > Render > Sampling > Render)
+    exposure = 1.5 # exposure of the entire image (UI: Scene > Render > Film > Exposure)
+    blenderInit(imgRes_x, imgRes_y, numSamples, exposure)
 
     # construct input and output paths
     meshPath = '../meshes/fish/fish'
@@ -107,6 +81,28 @@ for i in range(len(img_idx)):
     # using relative path gives us weired bug...
     setMat_texture(mesh, texturePath, meshColor)
 
+
+    ## set invisible plane (shadow catcher): control the brightness of the shadow
+    invisibleGround(shadowBrightness=0.8)
+
+    ## set camera (recommend to change mesh instead of camera, unless you want to adjust the Elevation)
+    camLocation = (2,1.85,0.32)
+    lookAtLocation = (0,0,0.5)
+    focalLength = 45 # (UI: click camera > Object Data > Focal Length)
+    cam = setCamera(camLocation, lookAtLocation, focalLength)
+    # bpy.context.object.data.type = 'ORTHO'
+
+    ## set light
+    ## Option1: Three Point Light System (recommended)
+    # setLight_threePoints(radius=4, height=10, intensity=2500, softness=8, keyLoc='left')
+    ## Option2: simple sun light
+    lightAngle = (-45,0,-45)
+    strength = 2
+    shadowSoftness = 1
+    sun = setLight_sun(lightAngle, strength, shadowSoftness)
+
+    ## set ambient light
+    setLight_ambient(color=(0.1,0.1,0.1,1)) # (UI: Scene > World > Surface > Color)
 
     ## set gray shadow to completely white with a threshold (optional)
     alphaThreshold = 0.05
